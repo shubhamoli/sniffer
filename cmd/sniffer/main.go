8b564804c9c33988a0fa73d6dbaa99d14db0d638
oli <oli.shubham@gmail.com>
2020-03-21 21:29:08 +0530

    Updated main.go

--- cmd/sniffer/main.go
+++ cmd/sniffer/main.go
@@ -4,25 +4,26 @@ import (
 	"fmt"
 	log "internal/logging"
 	"internal/config"
-	"internal/alert"
+	"internal/watcher"
 )
 
+func info(msg...string){
+	log.Logger.Info(msg)
+}
 
 func main() {
+	info("Starting Sniffer...")
+
 	Config, err := config.GetConfig()
 	if err != nil {
 		log.Logger.Fatal(fmt.Sprintf("Error in loading Configuration. Error: %s", err.Error()))
 	}
 
-	//_, err := config.ValidateConfig(Config)
-	//if err != nil {
-		//log.Logger.Fatal(fmt.Sprintf("Error while Validating Config. Error: %s", err.Error()))
-	//}
-
+	info(fmt.Sprintf("Sniffing %d items", len(Config.Sniff)))
 	for _, item := range Config.Sniff {
-		alert.SetupAlertForEntity(item)
+		watcher.Setup(item)
 	}
 
-	select {}      // this is a hack to run program forever
+	select {}      // this is a hack to run program forever, could've have used another solid solution.
 
 }
